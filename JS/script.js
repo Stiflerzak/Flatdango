b07af2157d0dc30c9ef1a80e48f99d19e4b12f7e
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-11 16:36:48 +0300

    added style

---
+++ JS/script.js
@@ -0,0 +1,18 @@
+// loads the dom before any other functionality
+document.addEventListener("DOMContentLoaded", () => {
+    // The endpoint to fetch data
+    const url= "http://localhost:3000/films"
+                    function getFilms(){    
+                    fetch(url) 
+                    .then(res => res.json()) 
+                    .then(data => console.log(data))
+                }
+                getFilms();
+    }) 
+              
+    
+    
+    
+    
+    
+ 
