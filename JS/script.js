33a739cd1bd22f2c24c2edd9dc780e04a85ebac2
Stiflerzak <hilarymwendia97@gmail.com>
2022-12-12 09:58:46 +0300

    added script functionality,fetch function,added data dynamically

--- JS/script.js
+++ JS/script.js
@@ -1,21 +1,47 @@
+
 // loads the dom before any other functionality
 document.addEventListener("DOMContentLoaded", () => {
+    getFilms()
+    }) 
+              
     // The endpoint to fetch data
     const url= "http://localhost:3000/films"
-                    function getFilms(){    
+    // function that does the actual fetching and convertion of data to json
+                function getFilms(){
+    
                     fetch(url) 
                     .then(res => res.json()) 
-                    .then(data => console.log(data))
+                    .then(data => {
+                       
+                        data.forEach(film => {
+                            displayfilms(film)
+                        });
+                    })
                 }
-                getFilms();
-    }) 
-       
-    const btn1= document.querySelector('#bookTicket');
-    btn1.addEventListener('click',() =>{
-        console.log('Someone Clicked me');
-    })
     
+    //this function displays Flatacuties animals
+                function displayfilms(film){
+                    const name= document.createElement('p')
+                    name.innerHTML=film.title
+                    const main= document.getElementById("main")
+                    main.append(name)
+                    //Event listener listens for a click, it the calls SingleCat function
+                    name.addEventListener("click", () => {
+                        singlefilm(film) 
+                        
+                    })
+                }
+            // This function handles all Flatacuties animal characteristics like Displaing the name, img and votes for each.
+    function singlefilm(film){
+        document.getElementById("movieTitle").innerHTML=film.title
+        document.getElementById("poster").src = film.poster
+        
     
+        
+    } 
+    
+
+
     
     
  
